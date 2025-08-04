29b2a987e88dcdeae7f772074e5b9b82cfea05cb
megumiss <zlj12138@gmail.com>
2025-08-05 01:59:51 +0800

    增加最小化到托盘按钮

--- webapp/electron/main.ts
+++ webapp/electron/main.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import path from 'path'
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, Tray, Menu, nativeImage, ipcMain} from 'electron'
 import {PyShell} from '../src/config/pyshell';
 import {webuiArgs} from '../src/config/config';
 import './ipc/WindowStrategy'
@@ -11,10 +11,12 @@ nkas.end(function (err: string) {
     console.log(err)
 });
 
+let tray: Tray | null = null; // 托盘对象
+let win: BrowserWindow | null = null;
+let isQuiting = false; // ← 用局部变量，不污染 app
+
 setTimeout(() => {
     const WinState = require('electron-win-state').default
-
-    let win: BrowserWindow | null = null;
 
     const winState = new WinState({
         defaultWidth: 1280,
@@ -42,7 +44,6 @@ setTimeout(() => {
         if (app.isPackaged) {
             win.loadFile(path.join(__dirname, '../../dist/index.html'))
             // win.webContents.openDevTools()
-
         } else {
             win.loadURL('http://localhost:5173/')
             win.webContents.openDevTools()
@@ -51,17 +52,58 @@ setTimeout(() => {
             win?.show();
         })
         winState.manage(win)
+    }
 
+    function createTray() {
+        const iconPath = path.join(__dirname, '../../dist/Helm.ico') // 使用现有图标
+        const trayIcon = nativeImage.createFromPath(iconPath);
+        tray = new Tray(trayIcon);
+
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: '显示窗口',
+                click: () => {
+                    win?.show();
+                    win?.focus();
+                }
+            },
+            {
+                label: '退出',
+                click: () => {
+                    isQuiting = true; // ← 退出时设置 isQuiting = true
+                    app.quit();
+                }
+            }
+        ]);
+        tray.setToolTip('NikkeAutoScript');
+        tray.setContextMenu(contextMenu);
+
+        tray.on('click', () => {
+            if (win?.isVisible()) {
+                win.hide();
+            } else {
+                win?.show();
+            }
+        });
     }
 
     app.whenReady().then(() => {
         createWindow();
-        app.on('activate', function () {
-            if (BrowserWindow.getAllWindows().length === 0) createWindow();
-        });
+        createTray();
     });
 
-    app.on('window-all-closed', function () {
-        if (process.platform !== 'darwin') app.quit();
-    })
-}, 8000)
+    app.on('window-all-closed', () => {
+        // 不退出应用，保持托盘运行
+    });
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+
+    ipcMain.on('minimize-to-tray', () => {
+        win?.hide();
+    });
+
+}, 0)
