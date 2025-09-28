4e820c97dbd13d13b1d6ec8f4808b140d31af106
megumiss <zlj12138@gmail.com>
2025-09-28 22:32:57 +0800

    修复协同作战过期检查

--- module/coop/coop.py
+++ module/coop/coop.py
@@ -50,7 +50,7 @@ class Coop(UI):
     @property
     def dateline(self) -> bool:
         date = self.coop_date
-        if date == '时间到' or '小时' not in date:
+        if date == '时间到' or '小时' or '剩余' not in date:
             logger.info('[Coop has expired]')
             return True
         else:
