175552ef0f4541f6681e63a482ab6388d2aef372
megumiss <zlj12138@gmail.com>
2025-10-28 18:12:17 +0800

    EN: Reward After Login

--- module/base/langs.py
+++ module/base/langs.py
@@ -16,6 +16,8 @@ class LangMeta(type):
         "COOP_TIMELINE_HOUR": {"en-US": "H", "zh-CN": "小时", "ja-JP": ""},
         "COOP_TIMELINE_LEFT": {"en-US": "Left", "zh-CN": "剩余", "ja-JP": ""},
         "COOP_TIMELINE_TIMEOUT": {"en-US": "Times Up", "zh-CN": "时间到", "ja-JP": ""},
+        # 其他
+        "CLAIM_ALL": {"en-US": "Claim All", "zh-CN": "全部领取", "ja-JP": ""},
     }
     _current_lang = "zh-CN"
 
